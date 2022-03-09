a47f4ce9b7f9905df94c03570e4320338b9fab58
jemma <ejayjeon@gmail.com>
2022-03-09 13:39:26 +0900

    0309 ALLOWED_HOSTS에 고정IP 세팅

--- web/web/settings.py
+++ web/web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-09=86^p%&gz2t!*yrv4r2lq+w3dhw3z^+wfucdjenunll#e+xe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.39.84.216']
 
 
 # Application definition
