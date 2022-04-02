c73776da1b1342468a3129575b3e073673ae273c
jemma <ejayjeon@gmail.com>
2022-04-02 21:22:12 +0900

    commit

--- worabel/ios/Runner/AppDelegate.swift
+++ worabel/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCQHHZDuIhXQtG35lyZK1rYxv46YpnxAJs")
+    GMSServices.provideAPIKey("YOUR KEY HEAR")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
