6f85976ac541cb24e122bd20ea21bb75c9f5facc
jemma <ejayjeon@gmail.com>
2023-01-26 19:42:35 +0900

    Kakao login

--- app/lib/login/kakao/components/kakao_login.dart
+++ app/lib/login/kakao/components/kakao_login.dart
@@ -1,10 +1,23 @@
 import 'package:app/common/layout/main_layout.dart';
 import 'package:app/login/kakao/providers/kakao_login_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 class KakaoLogin extends ConsumerWidget {
   const KakaoLogin({super.key});
+
+  void _get_user_info() async {
+    try {
+      User user = await UserApi.instance.me();
+      print('사용자 정보 요청 성공'
+          '\n회원번호: ${user.id}'
+          '\n닉네임: ${user.kakaoAccount?.profile?.nickname}');
+    } catch (error) {
+      print('사용자 정보 요청 실패 $error');
+    }
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -14,8 +27,39 @@ class KakaoLogin extends ConsumerWidget {
         child: Center(
           child: ElevatedButton(
             child: Text('로그인'),
-            onPressed: () {
-              state.checkInstall();
+            onPressed: () async {
+              // state.signInWithKakao();
+              if (await isKakaoTalkInstalled()) {
+                try {
+                  await UserApi.instance.loginWithKakaoTalk();
+                  print('카카오톡으로 로그인 성공');
+                  _get_user_info();
+                } catch (error) {
+                  print('카카오톡으로 로그인 실패 $error');
+
+                  // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                  // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                  if (error is PlatformException && error.code == 'CANCELED') {
+                    return;
+                  }
+                  // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
+                  try {
+                    await UserApi.instance.loginWithKakaoAccount();
+                    print('카카오계정으로 로그인 성공');
+                    _get_user_info();
+                  } catch (error) {
+                    print('카카오계정으로 로그인 실패 $error');
+                  }
+                }
+              } else {
+                try {
+                  await UserApi.instance.loginWithKakaoAccount();
+                  print('카카오계정으로 로그인 성공');
+                  _get_user_info();
+                } catch (error) {
+                  print('카카오계정으로 로그인 실패 $error');
+                }
+              }
             },
           ),
         ),
--- app/lib/login/kakao/providers/kakao_login_provider.dart
+++ app/lib/login/kakao/providers/kakao_login_provider.dart
@@ -1,15 +1,165 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:app/login/models/login_platform.dart';
+import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
-final kakaoLoginProvider = Provider<KakaoLogin>((ref) => KakaoLogin());
+final kakaoLoginProvider =
+    ChangeNotifierProvider((ref) => KakaoLogin(ref: ref));
 
-class KakaoLogin {
+class KakaoLogin extends ChangeNotifier {
+  final Ref ref;
+
+  final dio = Dio();
+
+  KakaoLogin({
+    required this.ref,
+  });
   // 1. 애플리케이션 install 체크
-  void checkInstall() async {
+  Future<OAuthToken> checkInstall() async {
     bool inInstalled = await isKakaoTalkInstalled();
 
     OAuthToken token = inInstalled
         ? await UserApi.instance.loginWithKakaoTalk()
         : await UserApi.instance.loginWithKakaoAccount();
+
+    print('[Token] $token');
+    return token;
+  }
+
+  void signInWithKakao() async {
+    try {
+      bool inInstalled = await isKakaoTalkInstalled();
+
+      OAuthToken token = inInstalled
+          ? await UserApi.instance.loginWithKakaoTalk()
+          : await UserApi.instance.loginWithKakaoAccount();
+      final url = Uri.https('kapi.kakao.com', '/v2/user/me');
+      final response = await http.get(
+        url,
+        headers: {
+          HttpHeaders.authorizationHeader: 'Bearer ${token.accessToken}'
+        },
+      );
+      final profileInfo = json.decode(response.body);
+      print(profileInfo.toString());
+    } catch (error) {
+      //
+    }
+  }
+}
+
+class SampleScreen extends ConsumerStatefulWidget {
+  const SampleScreen({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<SampleScreen> createState() => _SampleScreenState();
+}
+
+class _SampleScreenState extends ConsumerState<SampleScreen> {
+  LoginPlatform _loginPlatform = LoginPlatform.kakao;
+
+  void signInWithKakao() async {
+    try {
+      bool isInstalled = await isKakaoTalkInstalled();
+
+      OAuthToken token = isInstalled
+          ? await UserApi.instance.loginWithKakaoTalk()
+          : await UserApi.instance.loginWithKakaoAccount();
+
+      final url = Uri.https('kapi.kakao.com', '/v2/user/me');
+
+      final response = await http.get(
+        url,
+        headers: {
+          HttpHeaders.authorizationHeader: 'Bearer ${token.accessToken}'
+        },
+      );
+
+      final profileInfo = json.decode(response.body);
+      print(profileInfo.toString());
+
+      setState(() {
+        _loginPlatform = LoginPlatform.none;
+      });
+    } catch (error) {
+      print('카카오톡으로 로그인 실패 $error');
+    }
+  }
+
+  void signOut() async {
+    switch (_loginPlatform) {
+      case LoginPlatform.facebook:
+        break;
+      case LoginPlatform.google:
+        break;
+      case LoginPlatform.kakao:
+        await UserApi.instance.logout();
+        break;
+      case LoginPlatform.naver:
+        break;
+      case LoginPlatform.apple:
+        break;
+      case LoginPlatform.none:
+        break;
+    }
+
+    setState(() {
+      _loginPlatform = LoginPlatform.none;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: _loginPlatform != LoginPlatform.none
+              ? _logoutButton()
+              : Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _loginButton(
+                      'kakao_logo',
+                      signInWithKakao,
+                    )
+                  ],
+                )),
+    );
+  }
+
+  Widget _loginButton(String path, VoidCallback onTap) {
+    return Card(
+      elevation: 5.0,
+      shape: const CircleBorder(),
+      clipBehavior: Clip.antiAlias,
+      child: Ink.image(
+        image: AssetImage('asset/image/$path.png'),
+        width: 60,
+        height: 60,
+        child: InkWell(
+          borderRadius: const BorderRadius.all(
+            Radius.circular(35.0),
+          ),
+          onTap: onTap,
+        ),
+      ),
+    );
+  }
+
+  Widget _logoutButton() {
+    return ElevatedButton(
+      onPressed: signOut,
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(
+          const Color(0xff0165E1),
+        ),
+      ),
+      child: const Text('로그아웃'),
+    );
   }
 }
