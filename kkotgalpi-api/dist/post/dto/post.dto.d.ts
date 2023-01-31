ed0ba0243a067075275ed2132f11c7ce80478cf6
eunjeongJeon <ejayjeon@gmail.com>
2023-01-31 13:04:42 +0900

    a

--- kkotgalpi-api/dist/post/dto/post.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { Post } from '../model/post.model';
-declare const PostDto_base: import("@nestjs/common").Type<Pick<Post, "title" | "no" | "content" | "author" | "viewCount" | "created" | "updated">>;
-export declare class PostDto extends PostDto_base {
-}
-export {};
--- kkotgalpi-api/dist/post/post.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { PostService } from './post.service';
-import { PostDto } from './dto/post.dto';
-export declare class PostController {
-    private readonly postService;
-    constructor(postService: PostService);
-    create(no: number, postDto: PostDto): Promise<void>;
-}
